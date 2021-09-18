4adb52949b19aea7a32a63617fb86ae91141f623
Keder <kedernath.mallick.tint022@gmail.com>
2021-09-19 00:34:46 +0530

    project_added_01

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ProjectDet from './components/projects';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App ">
         <Navbar/>
         <section>
           <Home/>
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,34 +1,65 @@
-import { Typography } from '@mui/material';
-import React, { Component } from 'react';
-
+import React from 'react';
 import '../SASS/projectCard.scss'
-// const proj
+
+
+
+const proj=[
+    {
+        project_id:1,
+        project_name:"BlogIt",
+        project_type:"web_application",
+        project_url:"#",
+        Project_small_desc:"It is a simple Blog Apllication made in <strong>DJANGO</strong>.Any one can write a story.Go Ahead and make one.."
+    },
+    {
+        project_id:2,
+        project_name:"TellMeAJoke",
+        project_type:"web_extension",
+        project_url:"#",
+        Project_small_desc:"A simple web extension.It can tell you a funny joke and save you from a tedious works."
+    }
+]
 
 function ProjectDet(){
+    
     return(
         <React.Fragment>
             <div className="container col-xxl-8 px-2 py-5">
-                <div className="row px-1 py-1"></div>
-                    <div className="col-md-4">
-                        <div class="projCard">
-                            <div class="face face1">
-                            <div class="content">
-                                <span class="stars"></span>
-                                <h2 class="java">Java</h2>
-                                <p class="java">
-                                    Java is a class-based, object-oriented programming language 
-                                    that is designed to have as few implementation dependencies 
-                                    as possible.
-                                </p>
+                <h1 className="text-center display-5">My Projects</h1>
+                <br></br>
+                <br></br>
+                <div className="row px-1 py-1">
+                    {
+                        proj.map((pr)=>{
+                            console.log(pr);
+                            <div className="col-md-4">
+                                <div className="projCard">
+                                    <div className="face face1">
+                                        <div className="content card-body">
+                                            <span className="stars"></span>
+                                            <h2 className="backText text-center">
+                                                {pr.project_name}
+                                            </h2>
+                                            <br></br>
+                                            <p className="backText">
+                                                {pr.Project_small_desc}
+                                            </p>
+                                            <p className="linkProj">
+                                                <a href="#">Know More</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className="face face2">
+                                        <h2 className="h3 font-weight-bold text-center mb-4 heading">
+                                            {pr.project_name}
+                                        </h2>                               
+                                    </div>
+                                </div>
                             </div>
-                            </div>
-                            <div class="face face2">
-                            <h2>01</h2>
-                            </div>
-                        </div>
-                    </div>
-			     
-			    
+                        })
+                    }
+                    
+                </div>
             </div>
         </React.Fragment>
     )
